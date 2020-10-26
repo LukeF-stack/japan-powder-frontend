3d43d8dd8fe5835ed11889c42e6622ca134bf5d5
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2020-10-26 15:16:51 +0800

    google fonts implemented

--- src/components/HomePageHero.jsx
+++ src/components/HomePageHero.jsx
@@ -9,6 +9,7 @@ class HomePageHero extends React.Component {
         <section className="homePage-hero">
           <div className="hero-overlay"></div>
           <h1 className="homePage-hero-title">POWDER</h1>
+          <h6 className="homePage-hero-subtitle">JAPAN</h6>
         </section>
         <ParticlesBox />
       </div>
