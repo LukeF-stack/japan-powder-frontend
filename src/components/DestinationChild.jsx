a342fee8b1b5fef9ed0f62d934831294f2062443
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2020-10-27 16:34:17 +0800

    map points in general area - so far (4)

--- src/components/DestinationChild.jsx
+++ src/components/DestinationChild.jsx
@@ -3,10 +3,13 @@ import "../App.css";
 
 function DestinationChild(props) {
   const { destination, destinationId, displayInfo } = props;
+  const id = destination.toLowerCase().split(" ").join("_");
   const displayInfoParams = () => {
     displayInfo(destinationId);
   };
-  return <button onClick={displayInfoParams} className="map-point"></button>;
+  return (
+    <button onClick={displayInfoParams} className="map-point" id={id}></button>
+  );
 }
 
 export default DestinationChild;
