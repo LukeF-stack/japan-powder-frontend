24631fa3f8b78434cde4717d9b90eb34ee915fd7
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2020-11-01 14:20:24 +0800

    lightbox next button works

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -3,17 +3,38 @@ import "../App.css";
 
 function PhotoGallery(props) {
   const [gallery, setGallery] = useState([]);
+  const [photoList, setPhotoList] = useState({});
   const [activePhoto, setActivePhoto] = useState({});
   const [isShown, setShown] = useState(false);
   //const [destination, setDestination] = useState({});
   useEffect(() => {
     //console.log(props);
     getPhotos(props);
-  }, [props]);
+  }, []);
 
   const showModal = (photo) => {
+    //const photo = { index: photoIndex, src: photoSrc };
     setActivePhoto(photo);
     setShown(true);
+  };
+
+  const nextPhoto = () => {
+    //console.log(activePhoto);
+    //console.log(photoList);
+    // const photo = photoList.filter(function (result) {
+    //   if (activePhoto.index <= photoList.length + 1) {
+    //     return result.index === activePhoto.index + 1;
+    //   } else {
+    //     return result.index === "0";
+    //   }
+    // });
+    //console.log(photoList);
+    if (activePhoto.index < photoList.length - 1) {
+      setActivePhoto(photoList[activePhoto.index + 1]);
+    } else {
+      console.log("set to 0");
+      setActivePhoto(photoList[0]);
+    }
   };
 
   const getPhotos = async (props) => {
@@ -25,19 +46,30 @@ function PhotoGallery(props) {
       const destination = await response.json();
       //setDestination(destination);
       //console.log(destination);
+      const imageObjs = [];
       const images = [];
+      let index = 0;
       destination.photos.forEach((photo) => {
-        images.push(
-          <li key={photo} onClick={() => showModal(photo)}>
+        const photoObj = {};
+        photoObj["index"] = index;
+        photoObj["src"] = photo;
+        photoObj["el"] = (
+          <li key={index} onClick={() => showModal(photoObj)}>
             <div
               className="photo-result"
               style={{ backgroundImage: `url(${photo})` }}
             ></div>
           </li>
         );
+        //console.log(photoObj.index);
+        images.push(photoObj);
+        imageObjs.push(photoObj.el);
+        index += 1;
       });
-      setGallery(images);
-      setActivePhoto(images[0].key);
+      //console.log(images);
+      setPhotoList(images);
+      setGallery(imageObjs);
+      // setActivePhoto(images[0].src);
     } catch (e) {
       console.log(e.message);
     }
@@ -60,7 +92,23 @@ function PhotoGallery(props) {
 
       {isShown ? (
         <div className="gallery-modal-photo">
-          <img src={activePhoto} alt={activePhoto} className="displayed-img" />
+          <div className="modal_arrow">
+            <img src="/images/arrow_left.svg" alt="right arrow" />
+          </div>
+          <img
+            src={activePhoto.src}
+            alt={activePhoto.src}
+            className="displayed-img"
+          />
+          <div
+            className="modal_arrow"
+            onClick={() => {
+              //console.log(gallery);
+              nextPhoto();
+            }}
+          >
+            <img src="/images/arrow_right.svg" alt="right arrow" />
+          </div>
         </div>
       ) : null}
     </div>
