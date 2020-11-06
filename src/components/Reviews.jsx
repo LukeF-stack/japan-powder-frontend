e73398a1b2a1b22f9743667f5599043626fab182
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2020-11-06 21:05:15 +0800

    edit reviews, console log returns undefined

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -7,12 +7,14 @@ function Reviews(props) {
   const { user } = useContext(UserContext);
   const { id, state } = props;
   const [refresh, setRefresh] = useState({});
+  const [editedReview, setEditedReview] = useState({});
+  const [edit, setEdit] = useState(false);
 
   const [reviewResults, setReviewResults] = useState([]);
 
   useEffect(() => {
     getReviews();
-  }, [user, state, refresh]);
+  }, [user, state, refresh, edit]);
 
   const deleteReview = async (review_id) => {
     const settings = {
@@ -40,6 +42,61 @@ function Reviews(props) {
     }
   };
 
+  const createEditedReview = async () => {
+    //console.log(editedReview.message);
+    // const stamp = Date.now();
+    // const fields = {
+    //   Location_id: id,
+    //   Name: user.fullName,
+    //   Body: review.message,
+    //   User_id: user._id,
+    //   Timestamp: stamp
+    // };
+    // //console.log(fields);
+    // const settings = {
+    //   method: "POST",
+    //   headers: {
+    //     Accept: "application/json",
+    //     "Content-Type": "application/json"
+    //   },
+    //   body: JSON.stringify(fields)
+    // };
+    // try {
+    //   const response = await fetch(
+    //     `https://dsbn3.sse.codesandbox.io/api/reviews`,
+    //     settings
+    //   );
+    //   const data = await response.json();
+    //   //console.log(data.message);
+    //   if (!data) {
+    //     console.log("problem creating review");
+    //   } else if (data.msg) {
+    //     NotificationManager.error(data.msg);
+    //   } else {
+    //     //console.log("data is ", data);
+    //     NotificationManager.success(`Review successfully added`);
+    //     setShown(false);
+    //     setState(data);
+    //   }
+    // } catch (e) {
+    //   console.log(e.message);
+    //   //NotificationManager.error("Problem signing in user", "", 2000);
+    // }
+  };
+
+  const change = (e) => {
+    setEditedReview({
+      [e.target.name]: e.target.value
+    });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    createEditedReview();
+    setEditedReview({
+      message: ""
+    });
+  };
+
   const getReviews = async () => {
     try {
       const url = new URL(`https://dsbn3.sse.codesandbox.io/api/reviews`);
@@ -61,17 +118,51 @@ function Reviews(props) {
               {review.Timestamp ? (
                 <h6>{new Date(review.Timestamp).toDateString()}</h6>
               ) : null}
-              <p>{review.Body}</p>
+              {!edit ? (
+                <p>{review.Body}</p>
+              ) : (
+                <form action="">
+                  <textarea
+                    maxLength="800"
+                    name="message"
+                    type="text"
+                    value={editedReview.message}
+                    onChange={(e) => change(e)}
+                  />
+                </form>
+              )}
               {user._id === review.User_id ? (
                 <div>
-                  <button>edit</button>
-                  <button
-                    onClick={() => {
-                      deleteReview(review._id);
-                    }}
-                  >
-                    delete
-                  </button>
+                  {!edit ? (
+                    <button
+                      onClick={() => {
+                        setEdit(true);
+                        //setEditedReview({ message: review.Body });
+                      }}
+                    >
+                      edit
+                    </button>
+                  ) : (
+                    <button
+                      onClick={(e) => {
+                        //console.log(editedReview.message);
+                        onSubmit(e);
+                      }}
+                    >
+                      submit
+                    </button>
+                  )}
+                  {!edit ? (
+                    <button
+                      onClick={() => {
+                        deleteReview(review._id);
+                      }}
+                    >
+                      delete
+                    </button>
+                  ) : (
+                    <button>cancel</button>
+                  )}
                 </div>
               ) : null}
             </div>
