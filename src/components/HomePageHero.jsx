b6e8328152f80bb2e9b2b7e66fb4d2cd08425d72
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2020-11-10 14:24:56 +0800

    parallax looks hot

--- src/components/HomePageHero.jsx
+++ src/components/HomePageHero.jsx
@@ -8,12 +8,15 @@ class HomePageHero extends React.Component {
     return (
       <div>
         <section className="homePage-hero">
-          <div className="hero-parallax-wrapper-slow">
-            <Parallax speed={-8}>
-              <h1 className="homePage-hero-title">POWDER</h1>
-              <h6 className="homePage-hero-subtitle">JAPAN</h6>
-            </Parallax>
+          <div className="hero-background">
+            <div className="hero-parallax-wrapper-slow">
+              <Parallax speed={-8}>
+                <h1 className="homePage-hero-title">POWDER</h1>
+                <h6 className="homePage-hero-subtitle">JAPAN</h6>
+              </Parallax>
+            </div>
           </div>
+
           <div className="hero-parallax-wrapper-fast">
             <Parallax speed={3}>
               <div className="hero-overlay"></div>
