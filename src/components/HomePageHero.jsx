11a8ee6d348cc32086106947382aabe9dd861c43
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2020-11-10 13:37:23 +0800

    parallax implemented

--- src/components/HomePageHero.jsx
+++ src/components/HomePageHero.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import "../App.css";
 import ParticlesBox from "./Particles";
+import Parallax from "react-rellax";
 
 class HomePageHero extends React.Component {
   render() {
     return (
       <div>
         <section className="homePage-hero">
-          <div className="hero-overlay"></div>
-          <h1 className="homePage-hero-title">POWDER</h1>
-          <h6 className="homePage-hero-subtitle">JAPAN</h6>
+          <div className="hero-parallax-wrapper-slow">
+            <Parallax speed={-8}>
+              <h1 className="homePage-hero-title">POWDER</h1>
+              <h6 className="homePage-hero-subtitle">JAPAN</h6>
+            </Parallax>
+          </div>
+          <div className="hero-parallax-wrapper-fast">
+            <Parallax speed={3}>
+              <div className="hero-overlay"></div>
+            </Parallax>
+          </div>
         </section>
+
         <ParticlesBox />
       </div>
     );
