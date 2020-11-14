1c8f5d8df2930530053642f7bc7b78c7156d2740
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2020-11-14 14:35:38 +0800

    weathergrid re-layout start

--- src/components/DestinationInfo.jsx
+++ src/components/DestinationInfo.jsx
@@ -28,21 +28,23 @@ function DestinationInfo(props) {
           <h6>
             <strong>{island}</strong>
           </h6>
-          <div className="weather">
-            <h4>CURRENT WEATHER</h4>
-            <h3>
-              <strong>{currentWeather.main}</strong>
-            </h3>
-            <img
-              src={`http://openweathermap.org/img/wn/${currentWeather.icon}@2x.png`}
-              alt={currentWeather.main}
-            />
-            <h5>{currentWeather.description}</h5>
-            <h1>{currentWeather.temp}°C</h1>
-          </div>
-          <div>
-            <h4>WEEKLY FORECAST</h4>
-            <ul className="forecast-list">{forecast}</ul>
+          <div className="weather-grid">
+            <div className="weather">
+              <h4>CURRENT WEATHER</h4>
+              <h3>
+                <strong>{currentWeather.main}</strong>
+              </h3>
+              <img
+                src={`http://openweathermap.org/img/wn/${currentWeather.icon}@2x.png`}
+                alt={currentWeather.main}
+              />
+              <h5>{currentWeather.description}</h5>
+              <h1>{currentWeather.temp}°C</h1>
+            </div>
+            <div>
+              <h4>WEEKLY FORECAST</h4>
+              <ul className="forecast-list">{forecast}</ul>
+            </div>
           </div>
         </div>
       )}
