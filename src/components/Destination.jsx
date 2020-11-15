1c68b126c36dda19e1789640a6690862b68043a6
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2020-11-15 17:24:27 +0800

    map and buttons doesn't transition on state change

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -41,61 +41,101 @@ class DestinationParent extends React.Component {
   };
   render() {
     return (
-      <Transition timeout={200} in={this.state.bool}>
-        {(state) => (
-          <div
-            className="destination-bio"
-            style={{
-              ...defaultStyle,
-              ...transitionStyles[state]
-              // backgroundImage: `url(${this.state.bg_img})`
-            }}
-          >
-            <h1 className="map-title">
+      // <Transition timeout={200} in={this.state.bool}>
+      //   {(state) => (
+      <div
+        className="destination-bio"
+        // style={{
+        //   ...defaultStyle,
+        //   ...transitionStyles[state]
+        // }}
+      >
+        <Transition timeout={200} in={this.state.bool}>
+          {(state) => (
+            <h1
+              className="map-title"
+              style={{
+                ...defaultStyle,
+                ...transitionStyles[state]
+              }}
+            >
               {this.state.destinationTitle.toUpperCase()}
             </h1>
-            <p>{this.state.destinationDescription}</p>
-            <Link to={`/destinations/${this.state.destinationId}/info`}>
+          )}
+        </Transition>
+        <Transition timeout={200} in={this.state.bool}>
+          {(state) => (
+            <p
+              style={{
+                ...defaultStyle,
+                ...transitionStyles[state]
+              }}
+            >
+              {this.state.destinationDescription}
+            </p>
+          )}
+        </Transition>
+        <Transition timeout={200} in={this.state.bool}>
+          {(state) => (
+            <Link
+              to={`/destinations/${this.state.destinationId}/info`}
+              style={{
+                ...defaultStyle,
+                ...transitionStyles[state]
+              }}
+            >
               {this.state.buttonTitle ? (
                 <h5>{this.state.buttonTitle} &nbsp; &#x279C;</h5>
               ) : null}
             </Link>
-            <h6 className="island-description">
+          )}
+        </Transition>
+        <Transition timeout={200} in={this.state.bool}>
+          {(state) => (
+            <h6
+              className="island-description"
+              style={{
+                ...defaultStyle,
+                ...transitionStyles[state]
+              }}
+            >
               <strong>{this.state.island}</strong>
             </h6>
-            <div className="map-buttons">
-              <div className="map-img">
-                <img src="/images/japan_islands.svg" alt="map of japan" />
-              </div>
-              <div className="map-btn-wrapper">
-                <div className="map-btn-container">
-                  <DestinationChild
-                    destination="Hakuba Valley"
-                    destinationId="5f4f0c4d6dc6a63a00dfd95a"
-                    displayInfo={this.getInfo}
-                    open_weather_id={this.state.open_weather_id}
-                  />
-                  <DestinationChild
-                    destination="Niseko"
-                    destinationId="5f51ee44a99a94584080dc10"
-                    displayInfo={this.getInfo}
-                  />
-                  <DestinationChild
-                    destination="Shiga Kogen"
-                    destinationId="5f576091a066892294ead2de"
-                    displayInfo={this.getInfo}
-                  />
-                  <DestinationChild
-                    destination="Rusutsu"
-                    destinationId="5f5761d6a066892294ead2df"
-                    displayInfo={this.getInfo}
-                  />
-                </div>
-              </div>
+          )}
+        </Transition>
+        <div className="map-buttons">
+          <div className="map-img">
+            <img src="/images/japan_islands.svg" alt="map of japan" />
+          </div>
+          <div className="map-btn-wrapper">
+            <div className="map-btn-container">
+              <DestinationChild
+                destination="Hakuba Valley"
+                destinationId="5f4f0c4d6dc6a63a00dfd95a"
+                displayInfo={this.getInfo}
+                open_weather_id={this.state.open_weather_id}
+              />
+              <DestinationChild
+                destination="Niseko"
+                destinationId="5f51ee44a99a94584080dc10"
+                displayInfo={this.getInfo}
+              />
+              <DestinationChild
+                destination="Shiga Kogen"
+                destinationId="5f576091a066892294ead2de"
+                displayInfo={this.getInfo}
+              />
+              <DestinationChild
+                destination="Rusutsu"
+                destinationId="5f5761d6a066892294ead2df"
+                displayInfo={this.getInfo}
+              />
             </div>
           </div>
-        )}
-      </Transition>
+        </div>
+      </div>
+      //   )}
+      // </Transition>
     );
   }
 }
