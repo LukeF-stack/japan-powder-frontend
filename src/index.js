3fa787544fc575ee7076afb7753b336f8e18d189
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2020-11-17 19:25:41 +0800

    removed fonts.css and added !important to fonts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./fonts.css";
+
 import "./bonsai-base.min.css";
-import "./index.css";
 import "./App.css";
+import "./index.css";
 
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
